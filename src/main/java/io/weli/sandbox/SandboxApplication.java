1c88e4e47f3d38f29045fca6d42fa2fe16615741
weli <l.weinan@gmail.com>
2022-05-31 09:26:43 +0800

    iniit

---
+++ src/main/java/io/weli/sandbox/SandboxApplication.java
@@ -0,0 +1,13 @@
+package io.weli.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandboxApplication.class, args);
+	}
+
+}
